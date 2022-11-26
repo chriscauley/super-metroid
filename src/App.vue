399272027abb783849257e8fce6159d8c4dbb7dd
chriscauley <379151+chriscauley@users.noreply.github.com>
2022-11-26 10:04:48 -0500

    vue boilerplate

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    woo
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  lintOnSave: false,
+  devServer: {
+    port: 8079,
+    allowedHosts: ['.localhost'],
+    host: 'vclient.localhost',
+  }
+})
