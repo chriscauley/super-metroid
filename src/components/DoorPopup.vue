f74646e5af2a604239793ab5166e7ef90a8b1723
chriscauley <379151+chriscauley@users.noreply.github.com>
2022-11-27 11:51:24 -0500

    decomposing the data in the plando/tracker

---
+++ src/components/DoorPopup.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="doorPopup" class="doorPopup">
+    <table class="full">
+      <tr>
+        <td colspan="8"></td>
+      </tr>
+      <tr>
+        <td id="doorGrab" colspan="8" class="center greyRow">Choose door new color</td>
+      </tr>
+      <tr>
+        <td colspan="8" class="blankRow"></td>
+      </tr>
+      <tr>
+        <td v-for="color in door_colors" :key="color">
+          <div :id="`${color}DoorSelect`" @click="updateDoorColor(color)" />
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import { door_colors } from '@/data/old'
+import todo from '@/store/todo'
+
+export default {
+  data() {
+    return { door_colors }
+  },
+  methods: {
+    updateDoorColor: todo('DoorPopu.updateDoorColor'),
+  },
+}
+</script>
