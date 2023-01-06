4c99df8f279211b717aa2e50cbe7548180e456e9
chriscauley <379151+chriscauley@users.noreply.github.com>
2023-01-06 08:44:59 -0500

    adding some varia specific images

--- icons/src/App.vue
+++ icons/src/App.vue
@@ -1,50 +1,68 @@
 <template>
-  <div class="smi-tracker">
-    <div class="sm-item -missile" />
-    <div>
-      <div v-for="icon in inventory_icons" :key="icon" :class="icon" />
+  <div class="smi-tracker text-white p-8" style="font-size: 32px;">
+    <div v-for="icons, name in icon_groups" class="mb-8">
+      <h2>{{ name }}</h2>
+      <div  class="flex">
+      <div v-for="icon in icons" :key="icon" :class="icon" :title="icon" />
+    </div>
     </div>
   </div>
 </template>
 
 <script>
-const inventory_icons = [
-  'smi -auto',
-  'smi -energy',
-  'smi -ietank',
-  'smi -igrapple',
-  'smi -ixray',
-  'smi -imissile',
-  'smi -isuper',
-  'smi -ireserve',
-]
+
+const icon_groups = {
+  sm: [],
+  smv: [],
+  smi: [
+    'smi -auto',
+    'smi -energy',
+    'smi -ietank',
+    'smi -igrapple',
+    'smi -ixray',
+    'smi -imissile',
+    'smi -isuper',
+    'smi -ireserve',
+  ],
+  numbers: [],
+  reserve_numbers: [],
+}
+
 
 const bosses = ['ridley', 'draygon', 'phantoon', 'kraid']
-bosses.forEach((b) => inventory_icons.push('sm-item -' + b))
-bosses.forEach((b) => inventory_icons.push('sm-item -inactive -' + b))
+const minibosses = ['spore-spawn', 'botwoon', 'crocomire', 'golden-torizo']
+bosses.forEach((b) => icon_groups.sm.push('sm-item -' + b))
+bosses.forEach((b) => icon_groups.sm.push('sm-item -inactive -' + b))
+
+const all_bosses = [...bosses, ...minibosses, 'mother-brain']
+const levels = ['break', 'easy', 'medium', 'hard', 'harder', 'hardcore', 'mania']
+all_bosses.forEach(
+  (b, i) => icon_groups.smv.push(
+    `smv-boss -${b} smva-difficulty -difficulty-${levels[i%levels.length]}`
+  )
+)
 
 let i = 0
 while (i < 10) {
-  inventory_icons.push(`smi-number -number-${i}`)
+  icon_groups.numbers.push(`smi-number -number-${i}`)
   i++
 }
 i = 0
 while (i < 5) {
-  inventory_icons.push(`smi-reserve-number -number-${i}`)
+  icon_groups.reserve_numbers.push(`smi-reserve-number -number-${i}`)
   i++
 }
 
-const menu_items =
-  'charge-beam ice-beam wave-beam spazer-beam plasma-beam varia-suit gravity-suit morph-ball bomb spring-ball screw-attack hi-jump-boots space-jump speed-booster'.split(
-    ' ',
-  )
+// const beams = ['charge', 'ice', 'wave', 'spazer', 'plasma'].map(s => s+'-beam')
+// const suits = ['varia-suit', 'gravity-suit']
+// const misc = ['morph-ball', 'bomb', 'spring-ball', 'screw-attack']
+// const boots = ['hi-jump-boots', 'space-jump', 'speed-booster']
 
-menu_items.forEach((item) => inventory_icons.push(`smi-menu-item -${item}`))
-menu_items.forEach((item) => inventory_icons.push(`smi-menu-item -${item} -inactive`))
+// const menu_items = [ ...beams, ...suits, ...misc, ...misc]
 
 export default {
   data() {
-    return { inventory_icons }
+    return { icon_groups }
   },
 }
 </script>
