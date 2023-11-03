30bc4d397c9318679131274d91cdeeca873d34f5
chriscauley <379151+chriscauley@users.noreply.github.com>
2023-11-02 20:49:17 -0400

    removing some unused components

--- client/src/components/ItemTracker/index.js
+++
@@ -1,6 +0,0 @@
-import GridTracker from './GridTracker.vue'
-import PauseInventory from './PauseInventory.vue'
-
-export default {
-  components: { GridTracker, PauseInventory },
-}
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,26 +1,9 @@
 <template>
   <div>
-    <objective-tracker
-      :objectives="objectives"
-      :completed="completed"
-      @check="check"
-      @set-objectives="(o) => (objectives = o)"
-      @rm="rm"
-    />
   </div>
 </template>
 
 <script>
-import ObjectiveTracker from '@/components/ObjectiveTracker.vue'
 export default {
-  components: { ObjectiveTracker },
-  data() {
-    return { objectives: [], completed: {} }
-  },
-  methods: {
-    check(objective) {
-      this.completed[objective] = !this.completed[objective]
-    },
-  },
 }
 </script>
